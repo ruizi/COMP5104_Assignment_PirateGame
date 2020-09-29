6818d7d7df96905b3cd4cd85b6e64d77f19faa01
cai rui <rui.cai2020@outlook.com>
2020-09-29 23:18:36 +0800

    [Test] Test Case for Row53 and Row 67 (Captain card for doubling score)

--- Pirate/src/test/java/com/rui/pirate/testForPart1.java
+++ Pirate/src/test/java/com/rui/pirate/testForPart1.java
@@ -5,8 +5,11 @@ import com.rui.pirate.Client.Player;
 import com.rui.pirate.Game.GameService;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +24,17 @@ public class testForPart1 {
         String[] dieRoll = new String[]{"skull", "skull", "skull", "coin", "diamond", "diamond", "diamond", "diamond"};
         game = new GameService(true);
         assertTrue(player.isPlayerTurnDie(card, dieRoll, game));
+    }
+
+    @Test
+    public void testForRow53() {//score first roll with nothing but 2 diamonds and 2 coins and FC is captain (SC 800)
+        fortuneCard = "Captain";
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"monkey", "monkey", "parrot", "diamond", "diamond", "coin", "coin", "parrot"};
+        String input = "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(800, player.playerRound(card, dieRoll, game));
     }
 
     @Test
@@ -50,11 +64,27 @@ public class testForPart1 {
     }
 
     @Test
+    public void testForRow67() {//score set of 8 swords on first roll and FC is captain (SC 4500x2 = 9000) if you have full chest
+        fortuneCard = "Captain";
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"sword", "sword", "sword", "sword", "sword", "sword", "sword", "sword"};
+        String input = "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(9000, player.playerRound(card, dieRoll, game));
+    }
+
+    @Test
     public void testForRow75() {//get 7 swords on first roll, try to roll the 8 die by itself -> interface reports not allowed
         String[] dieRoll = new String[]{"sword", "sword", "sword", "sword", "sword", "sword", "sword", "monkey"};
         game = new GameService(true);
         ArrayList<Integer> skullDice = game.locateSkull(dieRoll);
         ArrayList<Integer> heldDice = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
         assertFalse(game.heldDiceValidCheck(skullDice, heldDice));
+    }
+
+    public Scanner setTestInputScanner(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());//可以通过调用System.setIn（InputStream in）来用自己的流替换System.in。InputStream可以是一个字节数组
+        return new Scanner(in);
     }
 }
