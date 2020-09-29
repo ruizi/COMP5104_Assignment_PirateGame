ce3ca5d80d07e69efdec65a680e5a62b84b989cf
cai rui <rui.cai2020@outlook.com>
2020-09-30 00:21:13 +0800

    [Test] Test Case immediately Passed for Row56,Row 57,Row59 to Row65 with the code phrase (re-roll situation 1)

--- Pirate/src/test/java/com/rui/pirate/testForPart1.java
+++ Pirate/src/test/java/com/rui/pirate/testForPart1.java
@@ -99,6 +99,26 @@ public class testForPart1 {
     }
 
     @Test
+    public void testForRow56() { //score 2 sets of 3 (monkey, parrots) in RTS using 2 rolls   (SC 300)
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"monkey", "monkey", "parrot", "parrot", "sword", "sword", "skull", "skull"};
+        String input = "2 " + "1,2,3,4 " + "monkey,sword " + "2 " + "1,2,3,4 " + "monkey,parrot " + "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(300, player.playerRound(card, dieRoll, game));
+    }
+
+    @Test
+    public void testForRow57() { //score a set of 3 diamonds correctly (i.e., 400 points)   (SC 500)
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"diamond", "diamond", "diamond", "monkey", "monkey", "parrot", "sword", "sword"};
+        String input = "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(500, player.playerRound(card, dieRoll, game));
+    }
+
+    @Test
     public void testForRow58() { //score a set of 4 coins correctly (i.e., 200 + 400 points) with FC is a diamond (SC 700)
         fortuneCard = "Diamond";
         Card card = new Card(fortuneCard);
@@ -108,6 +128,78 @@ public class testForPart1 {
         game = new GameService(true, scanner);
         assertEquals(700, player.playerRound(card, dieRoll, game));
     }
+
+    @Test
+    public void testForRow59() { //score set of 3 swords and set of 4 parrots correctly on first roll (SC 400 because of FC)
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"sword", "sword", "sword", "parrot", "parrot", "parrot", "parrot", "monkey"};
+        String input = "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(400, player.playerRound(card, dieRoll, game));
+    }
+
+    @Test
+    public void testForRow60() { //score set of 3 coins+ FC and set of 4 swords correctly over several rolls (SC = 200+400+200 = 800)
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"coin", "coin", "sword", "sword", "parrot", "parrot", "monkey", "monkey"};
+        String input = "2 " + "1,2,3,4 " + "sword,parrot,monkey,monkey " + "2 " + "1,2,3,4,5 " + "sword,parrot,coin " + "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(800, player.playerRound(card, dieRoll, game));
+    }
+
+    @Test
+    public void testForRow61() {//same as previous row but with captain fortune card  (SC = (100 + + 300 + 200)*2 = 1200)
+        fortuneCard = "Captain";
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"coin", "coin", "sword", "sword", "parrot", "parrot", "monkey", "monkey"};
+        String input = "2 " + "1,2,3,4 " + "sword,parrot,monkey,monkey " + "2 " + "1,2,3,4,5 " + "sword,parrot,coin " + "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(1200, player.playerRound(card, dieRoll, game));
+    }
+
+    @Test
+    public void testForRow62() { //score set of 5 swords over 3 rolls (SC 600)
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"sword", "sword", "sword", "parrot", "monkey", "parrot", "skull", "monkey"};
+        String input = "2 " + "1,2,3 " + "sword,monkey,parrot,monkey " + "2 " + "1,2,3,4 " + "sword,parrot,monkey " + "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(600, player.playerRound(card, dieRoll, game));
+    }
+
+    @Test
+    public void testForRow63() { //score set of 6 monkeys on first roll (SC 1100)
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"monkey", "monkey", "monkey", "monkey", "monkey", "monkey", "skull", "parrot"};
+        String input = "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(1100, player.playerRound(card, dieRoll, game));
+    }
+
+    @Test
+    public void testForRow64() {//score set of 7 parrots on first roll (SC 2100)
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"parrot", "parrot", "parrot", "parrot", "parrot", "parrot", "parrot", "monkey"};
+        String input = "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(2100, player.playerRound(card, dieRoll, game));
+    }
+
+    @Test
+    public void testForRow65() { //score set of 8 coins on first roll (SC 5400)  seq of 8 + 9 coins +  full chest (if you have it)
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"coin", "coin", "coin", "coin", "coin", "coin", "coin", "coin"};
+        String input = "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(5400, player.playerRound(card, dieRoll, game));
+    }
+
 
     @Test
     public void testForRow66() {//score set of 8 coins on first roll and FC is diamond (SC 5400)
