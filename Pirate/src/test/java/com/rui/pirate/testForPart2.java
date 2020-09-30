5038bec873f56515b9e2c4b5872e3cc43c817d2a
cai rui <rui.cai2020@outlook.com>
2020-09-30 14:25:48 +0800

    [Test] Test Case for Row80 to Row 82 (Sorceress)

--- Pirate/src/test/java/com/rui/pirate/testForPart2.java
+++ Pirate/src/test/java/com/rui/pirate/testForPart2.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.Assert.assertEquals;
@@ -17,6 +19,51 @@ public class testForPart2 {
     public final Player player = new Player("Test");
     public String fortuneCard = "Gold";
     GameService game = new GameService(true);
+
+    @Test
+    public void testForRow80() { //roll 2 skulls, re-roll one of them due to sorceress, then go to next round of turn。
+        fortuneCard = "Sorceress";
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"coin", "parrot", "parrot", "parrot", "parrot", "sword", "skull", "skull"};
+        String input = "7 " + "1,2,3,4,5 " + "sword,sword ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        ArrayList<Integer> skullDice = game.locateSkull(dieRoll);
+        assertEquals(2, skullDice.size());
+        card.sorceress.sorceressCard(skullDice, dieRoll, game);
+        assertTrue(card.sorceress.isUsed());
+        assertEquals(1, skullDice.size());
+    }
+
+    @Test
+    public void testForRow81() { //roll no skulls, then next round roll 1 skull and re-roll for it, then score
+        fortuneCard = "Sorceress";
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"coin", "parrot", "parrot", "parrot", "parrot", "sword", "sword", "sword"};
+        String input = "2 " + "1,2,3,4,5 " + "skull,sword,sword " + "3 " + "6 " + "1,2,3,4,5 " + "monkey,sword,sword " + "1 ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        assertEquals(300, player.playerRound(card, dieRoll, game));
+    }
+
+    @Test
+    public void testForRow82() { //roll no skulls, then next round roll 1 skull and re-roll for it, then go to next round
+        fortuneCard = "Sorceress";
+        Card card = new Card(fortuneCard);
+        String[] dieRoll = new String[]{"coin", "parrot", "parrot", "parrot", "parrot", "sword", "sword", "sword"};
+        String input = "skull,sword,sword " + "6 " + "1,2,3,4,5 " + "monkey,sword,sword ";
+        Scanner scanner = setTestInputScanner(input);
+        game = new GameService(true, scanner);
+        ArrayList<Integer> held = new ArrayList<>(List.of(0, 1, 2, 3, 4));
+        ArrayList<Integer> treasureChest = new ArrayList<>();
+        ArrayList<Integer> skullDice = game.locateSkull(dieRoll);
+        game.reRollNotHeld(dieRoll, held, skullDice, treasureChest);
+        game.printDieRoll(dieRoll);
+        card.sorceress.sorceressCard(skullDice, dieRoll, game);//新一轮的投掷情况
+        skullDice = game.locateSkull(dieRoll);
+        game.printDieRoll(dieRoll);
+        assertEquals(0, skullDice.size());
+    }
 
     @Test
     public void testForRow90() {//roll 3 parrots, 2 swords, 2 diamonds, 1 coin    put 2 diamonds and 1 coin in chest
